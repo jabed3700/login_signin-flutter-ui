a10d2eb8e7c5d4a2759e03e0c193d69d46a12c8e
tamim08 <tamim008bd@gmail.com>
2021-07-06 07:46:53 +0600

    git init

--- lib/Screens/Welcome/welcome_screen.dart
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'component/body.dart';
+
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      body: Body(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login/Screens/Welcome/welcome_screen.dart';
+import 'package:login/constant.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Login',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
       home: WelcomeScreen(),
     );
